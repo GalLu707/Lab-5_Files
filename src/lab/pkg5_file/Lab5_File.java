8f40ba2d197187fc7e82d790ce4ad86c629c4440
GalLu707 <zasseuwu@gmail.com>
2026-02-27 08:13:15 -0600

    repo creado

---
+++ src/lab/pkg5_file/Lab5_File.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab.pkg5_file;
+
+public class Lab5_File {
+
+    
+    public static void main(String[] args) {
+       //otro lab para sufrir
+    }
+    
+}
